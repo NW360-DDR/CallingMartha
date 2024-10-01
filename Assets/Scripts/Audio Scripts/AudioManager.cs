d8304351659be0beec5ca78eeb0f12722d233814
Ronan-Cords <71858463+Ronan-Cords@users.noreply.github.com>
2024-10-01 13:55:44 -0400

    AudioManager Changes

--- Assets/Scripts/Audio Scripts/AudioManager.cs
+++ Assets/Scripts/Audio Scripts/AudioManager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
-    public AudioClip[] AudioClips;
     // Start is called before the first frame update
     void Start()
     {
