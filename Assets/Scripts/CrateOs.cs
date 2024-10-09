9a4188521deaca70aadb44f057184bbcda421d16
Ronan-Cords <71858463+Ronan-Cords@users.noreply.github.com>
2024-10-09 09:18:51 -0400

    Revert "Useless Fake Commit"
    This reverts commit 1827251e3b6c46b720c8ccde57f6f4b0b28538ce.

--- Assets/Scripts/CrateOs.cs
+++ Assets/Scripts/CrateOs.cs
@@ -12,6 +12,5 @@ public class CrateOs : MonoBehaviour
     {
             Vector3 TorqueDir = new(rot[Random.Range(0, rot.Length)], rot[Random.Range(0, rot.Length)], rot[Random.Range(0, rot.Length)]);
             transform.rotation = Quaternion.Euler(TorqueDir);
-        Debug.Log("What are you doing here get out of my house I am calling the police you need to leave.");
     }
 }
