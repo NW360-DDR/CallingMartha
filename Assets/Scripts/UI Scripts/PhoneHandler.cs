e28495d14c58ddd854ad6d3c530b63eb6ef16475
Nate <rushlord42@gmail.com>
2024-10-11 11:05:23 -0400

    Removed GPS, Simplified HUD screen, Code Cleanup Part 1

--- Assets/Scripts/UI Scripts/PhoneHandler.cs
+++ Assets/Scripts/UI Scripts/PhoneHandler.cs
@@ -6,67 +6,39 @@ using TMPro;
 
 public class PhoneHandler : MonoBehaviour
 {
-   const KeyCode Z = KeyCode.Z, C = KeyCode.C;
-    enum Screen {GPS, VM, Log, HUD};
-    [SerializeField] GameObject GPSMode;
-    [SerializeField] GameObject VoicemailMode;
-    [SerializeField] GameObject LogMode;
+    enum Screen {HUD, Save};
+    [SerializeField] GameObject SaveMode;
     [SerializeField] GameObject HUDMode;
-    [SerializeField] RawImage HPBar, BattBar;
-    float barWidth;
     [SerializeField] TextMeshProUGUI rockText, battText, kitText;
     CellService cell;
     public string playerName;
     Player player;
-    int[] temp;
+    int[] inventoryTemp;
     Screen currentScreen = Screen.HUD;
     // Start is called before the first frame update, used to find anything we need in our Components/
     void Start()
     {
         cell = GetComponentInChildren<CellService>();
-        barWidth = HPBar.rectTransform.rect.width;
         if (string.IsNullOrWhiteSpace(playerName))
             player = new Player();
         else
             player = new Player(playerName);
-        temp = new int[3];
+        inventoryTemp = new int[3];
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(Z))
-        { // For now, Z just enables the HUD, and C enables the minimap
-            SwitchMode(Screen.HUD);
-        }
-        else if (Input.GetKeyDown(C))
-        {
-            SwitchMode(Screen.GPS);
-        }
-
-    }
     private void FixedUpdate()
     {
-        if (currentScreen == Screen.HUD)
-        {
-            temp = player.GetInventory(); // Remember, Rocks, Batts, Kits.
-            rockText.text = temp[0].ToString();
-            battText.text = temp[1].ToString();
-            kitText.text = temp[2].ToString();
-            HPBar.rectTransform.sizeDelta = new(barWidth * (player.GetHealth() / 100f), HPBar.rectTransform.sizeDelta.y);
-            BattBar.rectTransform.sizeDelta = new(barWidth * (player.GetBatt() / 100f), BattBar.rectTransform.sizeDelta.y);
-        }
+        inventoryTemp = player.GetInventory(); // Remember, Rocks, Batts, Kits.
+        rockText.text = inventoryTemp[0].ToString();
+        battText.text = inventoryTemp[1].ToString();
+        kitText.text = inventoryTemp[2].ToString();
     }
     void SwitchMode(Screen newMode)
     {
-        GPSMode.SetActive(false);
-        VoicemailMode.SetActive(false);
-        LogMode.SetActive(false);
         HUDMode.SetActive(false);
         switch (newMode) 
         {
-            case Screen.GPS:
-                GPSMode.SetActive(true);
-                break;
+
             case Screen.HUD:
                 HUDMode.SetActive(true);
                 break;
@@ -85,45 +57,31 @@ public class PhoneHandler : MonoBehaviour
     }
 }
 
+/// <summary>
+/// Converts most of the information from the player's various scripts into a single, easy to parse class.
+/// </summary>
 public class Player{
      readonly GameObject main;
      public GrabAndThrow inventory;
-     readonly HealthAndRespawn health;
-    readonly FlashlightScript light;
-    
-
     public Player() 
     {
         Debug.Log("No name specified, default constructor.");
         main = GameObject.FindWithTag("Player");
         inventory = main.GetComponentInChildren<GrabAndThrow>();
-        health = main.GetComponent<HealthAndRespawn>();
-        light = main.GetComponent<FlashlightScript>();
         
     }
     public Player(string playerName)
     {
         main = GameObject.Find(playerName);
         inventory = main.GetComponentInChildren<GrabAndThrow>();
-        health = main.GetComponent<HealthAndRespawn>();
-        light = main.GetComponent<FlashlightScript>();
-    }
-    public int GetHealth()
-    {
-        return health.health; // Why is health a float? Do we need decimal accuracy when we only add or remove in integer increments?
     }
 
-    public float GetBatt()
-    {
-        return light.batteryLife;
-    }
-
-    public int[] GetInventory()
+    public int[] GetInventory() // Returns the players Rocks, Batteries, and Medkit counts for the UI.
     {
         int[] temp = { 3, 4, 5 }; // Rocks, Batts, Kits
-        temp[0] = inventory.rockCount; // WHO HOLDS A DECIMAL NUMBER OF ROCKS
-        temp[1] = inventory.flashLightBatteries; // okay this one is almost debatable but what the fuck // This isn't in my version of the script yet, roll with it.
-        temp[2] = inventory.medKitCount; // WE AREN'T USING HALF A MEDKIT
+        temp[0] = inventory.rockCount;
+        temp[1] = inventory.flashLightBatteries;
+        temp[2] = inventory.medKitCount;
 
         return temp;
     }
