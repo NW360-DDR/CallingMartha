ae77b687fce696464d09849537f23494d21dbade
Nate <rushlord42@gmail.com>
2024-10-17 08:37:52 -0400

    The Single Most Glorious Main Menu Prototype Ever To Yes

---
+++ Assets/MainMenuHandler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public string MainGame;
+    bool prologueStarted;
+    public float scrollSpeed = 0.0f;
+    public TextMeshProUGUI text;
+    public GameObject skip;
+
+     void Update()
+    {
+        if (prologueStarted)
+        {
+            text.rectTransform.Translate(Time.deltaTime * scrollSpeed * Vector2.up);
+            if (Input.GetKeyDown(KeyCode.E) || text.rectTransform.position.y >= 0)
+            {
+                StartGame();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(MainGame);
+    }
+
+    public void Prologue()
+    {
+        prologueStarted = true;
+        skip.gameObject.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
