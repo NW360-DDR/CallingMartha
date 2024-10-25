036fb2bcdfe64256a6a2323bb5549553cb7086e0
LunaFreya64 <chrisvisconte@gmail.com>
2024-10-25 11:47:57 -0400

    everything should be okay?

--- Assets/Scripts/Player Remake/EquippedScript.cs
+++ Assets/Scripts/Player Remake/EquippedScript.cs
@@ -7,11 +7,18 @@ public class EquippedScript : MonoBehaviour
     public GameObject weapon;
     private Animator weaponAnim;
 
+    private InventoryScript inventoryScript;
+    private GunScript gunScript;
+    private AxeSlash axeScript;
+
     public int currentEquipped = 0;
     // Start is called before the first frame update
     void Start()
     {
         weaponAnim = weapon.GetComponent<Animator>();
+        inventoryScript = GetComponent<InventoryScript>();
+        gunScript = GetComponent<GunScript>();
+        axeScript = GetComponent<AxeSlash>();
     }
 
     // Update is called once per frame
@@ -20,13 +27,15 @@ public class EquippedScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1) && currentEquipped != 0)
         {
             currentEquipped = 0;
+            gunScript.enabled = false;
             weaponAnim.SetTrigger("ToAxe");
             weaponAnim.SetBool("SwitchingHand", true);
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2) && currentEquipped != 1)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && currentEquipped != 1 && inventoryScript.bulletCount > 0)
         {
             currentEquipped = 1;
+            gunScript.enabled = true;
             weaponAnim.SetTrigger("ToGun");
             weaponAnim.SetBool("SwitchingHand", true);
         }
