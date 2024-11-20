4a65508e2016e8b56dd5c1a9d70964ab1f549321
LunaFreya64 <chrisvisconte@gmail.com>
2024-11-20 10:40:14 -0500

    fixed being able to break down blocked doors through the door

--- Assets/Scripts/Interactable Scripts/BoatScript.cs
+++ Assets/Scripts/Interactable Scripts/BoatScript.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class BoatScript : Interactable
 {
     private HealthAndRespawn healthScript;
+    public EclipseTimer eclipseTimer;
     public GameObject teleportPos;
+    private GameObject player;
     void Interact()
     {
-        healthScript = GameObject.Find("Player (Remake)").GetComponent<HealthAndRespawn>();
+        player = GameObject.Find("Player (Remake)");
+        healthScript = player.GetComponent<HealthAndRespawn>();
 
         healthScript.blackScreen.SetBool("FadeIn", true);
+        eclipseTimer.enteredArena = true;
         StartCoroutine(MovePlayer());
     }
 
     IEnumerator MovePlayer()
     {
         yield return new WaitForSeconds(1);
-        GameObject.Find("Player (Remake)").transform.position = teleportPos.transform.position;
+        player.transform.position = teleportPos.transform.position;
         healthScript.blackScreen.SetBool("FadeIn", false);
     }
 }
--- Assets/Scripts/Player Remake/AxeSlash.cs
+++ Assets/Scripts/Player Remake/AxeSlash.cs
@@ -7,8 +7,11 @@ public class AxeSlash : MonoBehaviour
     public GameObject weapon;
 
     private EquippedScript equipScript;
+    public Camera cam;
+    private RaycastHit checkForBreakable;
     private Animator axeAnim;
     public bool attackSignal = false;
+    public LayerMask excludeLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +36,7 @@ public class AxeSlash : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && equipScript.allowAttack && Time.timeScale > 0)
         {
             axeAnim.SetTrigger("Attack");
+            BreakableCheck();
         }
     }
 
@@ -44,5 +48,20 @@ public class AxeSlash : MonoBehaviour
     void TurnOffHitbox()
     {
         hitBox.SetActive(false);
+    }
+
+    void BreakableCheck()
+    {
+        Physics.Raycast(cam.transform.position, cam.transform.forward, out checkForBreakable, 5, ~excludeLayer);
+
+        if (checkForBreakable.transform != null)
+        {
+            if (checkForBreakable.transform.CompareTag("Breakable"))
+            {
+                Debug.Log("Can be broken!");
+
+                Destroy(checkForBreakable.transform.gameObject);
+            }
+        }
     }
 }
