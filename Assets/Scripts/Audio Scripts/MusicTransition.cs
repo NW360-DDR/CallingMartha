ae5bb0a901868a8cb6d004f8a4a2718e128f18ec
Ronan-Cords <rcords@student.fitchburgstate.edu>
2024-12-04 16:53:41 -0500

    Music Looping Continued

---
+++ Assets/Scripts/Audio Scripts/MusicTransition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTransition : MonoBehaviour
+{
+
+    public FMOD.Studio.EventInstance ExplorationMusic;
+    public FMOD.Studio.EventInstance EncounterMusic;
+    public FMOD.Studio.EventInstance BossMusic;
+
+    public bool ExplorationPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExplorationMusic = FMODUnity.RuntimeManager.CreateInstance("events:/Music/PlayExploration");
+        EncounterMusic = FMODUnity.RuntimeManager.CreateInstance("events:/Music/PlayEncounter");
+        BossMusic = FMODUnity.RuntimeManager.CreateInstance("events:/Music/PlayBoss");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ExplorationPlaying = false)
+        {
+            ExplorationMusic.start();
+            ExplorationPlaying = true;
+        }
+    }
+}
