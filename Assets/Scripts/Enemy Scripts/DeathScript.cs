d47f4d8f4f9e8952ebbccad8f88bf9703985ca6e
Nate <rushlord42@gmail.com>
2024-12-12 13:54:32 -0500

    Actual final push I hate everything

--- Assets/Scripts/Enemy Scripts/DeathScript.cs
+++ Assets/Scripts/Enemy Scripts/DeathScript.cs
@@ -24,7 +24,7 @@ public class DeathScript : MonoBehaviour
 
     IEnumerator StopAllMoving()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(2f);
         Destroy(rb);
         Destroy(collider);
     }
